4437f0080c46e668f181617e358609e534b04a80
Ross Mattheis <ramattheis@gmail.com>
2018-04-26 23:17:11 -0700

    Updating text cleaning code

--- Ross/code/cleaning_text_data.r
+++ Ross/code/cleaning_text_data.r
@@ -7,11 +7,23 @@ setwd("~/Dropbox/Misc/lib_project/data-science-fellowship/Ross/data/gocr_pgm_txt
 
 files <- dir() 
 
+# initializing data frame 
+txtdata <- read.table("electionresults-024.txt", sep = "\n")
+colnames(txtdata) <- "text"
+txtdata$page <- as.integer(gsub("[^0-9]","",file))
+txtdata$method <- "gocr"
+txtdata <- txtdata[-c(1:(nrow(txtdata))),]
+
+# extract
 for(file in files){
-  text <- read.table("electionresults-024.txt", row = 1)
+  text <- read.table(eval(file), sep = "\n")
   colnames(text) <- "text"
-  text$page <- 
+  if(nrow(text) > 0){
+  text$page <- as.integer(gsub("[^0-9]","",file))
   text$method <- "gocr"
+  txtdata <- rbind(txtdata,text)
+  rm(text)
+  }
 }
 
 text <- read.table("electionresults-024.txt", row = 1) 
